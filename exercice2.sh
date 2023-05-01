b5bb55f3f0458005dc54dc27872c2bcb7edd5f98
Henrick Baril <Barilhenrick@gmail.com>
2023-05-01 11:44:35 -0400

    ajout de code

--- exercice2.sh
+++ exercice2.sh
@@ -1,1 +1,25 @@
 #!/bin/bash
+
+#1- Afficher un menu
+#2- Mettre le menu dans une fonction
+#3- Faire une boucle
+#   -Offrire 2 choix : quitter (q) ou appelez ./exercise1.sh (d)
+#4- À chaque début de boucle, afficher le menu
+
+
+maFonction() {
+    echo -e "Menu\n______\nQuitter (q)\nAfficher (d)" 
+    read choix
+    if [[ $choix = d ]]
+    then 
+        clear
+        ./exercice1.sh
+    fi
+}
+
+while [[ $choix != q ]]
+do
+    clear
+    maFonction
+    sleep 5
+done
