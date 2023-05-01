358f2dd1d47d39355aea5d55173b36db191df2ef
Henrick Baril <Barilhenrick@gmail.com>
2023-05-01 09:47:10 -0400

    changement du code et ajout d'exemple

--- 01-hello_world.sh
+++ 01-hello_world.sh
@@ -1,3 +1,16 @@
 #!/bin/bash
 
 echo "Hello" $USER
+echo -n "Vive "
+echo "Linux"
+
+#exit -> permet de quitter le programme.
+
+# -e -> permet d'interpreter les retours de ligne et etc (\n, \t, ...).
+echo -e "1\n2\n3"
+
+read -p "Quel age avez-vous ? " age  #read -> permet de lire l'input
+echo $age " ! vous etes jeune"
+
+# Pour mettre dans une variable il ne faut pas mettre de $ 
+# tandis que pour l'appeler oui.
