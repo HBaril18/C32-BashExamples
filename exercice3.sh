12a09fe5cf76353d87df9578e57cd868399b6c9c
Henrick Baril <Barilhenrick@gmail.com>
2023-05-01 12:21:33 -0400

    ajout de code

--- exercice3.sh
+++ exercice3.sh
@@ -1,1 +1,22 @@
 #!/bin/bash
+
+espace() {
+    for ((i=0;i<$1;i++))
+    do
+        echo -n " "
+    done
+}
+
+for ((i=0;i<7;i++))
+do
+    clear
+    espace $i
+    if [[ $i -eq 6 ]]
+    then 
+        echo "BOOM"
+    else
+        echo "@"
+    fi
+    sleep 0.5
+    clear
+done
