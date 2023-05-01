d310e80938886a07c0e7493507d572c0e50113e7
Henrick Baril <Barilhenrick@gmail.com>
2023-05-01 09:57:01 -0400

    Ajout de code

--- 02-variables.sh
+++ 02-variables.sh
@@ -1,2 +1,11 @@
 #!/bin/bash
 
+prenom=george                # NE PAS METTRE D'ESPACE POUR METTRE GEORGE DANS LA VARIABLE
+echo $prenom
+
+declare -i age=33            # declare -> permet de déclarer une variable avec un type précis
+declare -r CHEMIN=/bin       # -r -> permet de la mettre en "Constante"
+
+echo "Nom script" $0         # $0 -> donne le nom du script
+echo "Nombre parametres" $#  # $# -> donne le nombre de parametres passer à la variable
+echo "Perso : " $1           # $1 -> donne le nom du premier parametre
