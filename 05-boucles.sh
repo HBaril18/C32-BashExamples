8c8267e79b1708e6882744fab1af4c35c93478d5
Henrick Baril <Barilhenrick@gmail.com>
2023-05-01 10:43:51 -0400

    ajout de code

--- 05-boucles.sh
+++ 05-boucles.sh
@@ -1,2 +1,22 @@
 #!/bin/bash
 
+for ((i=0;i<5;i++))
+do
+    echo $i
+done
+
+# `` -> exécution d'une commande et le met dans la variable
+
+liste=`ls`
+
+for fichier in $liste
+do
+    echo $fichier
+done
+
+resultat=o
+
+while [[ $resultat = o ]]
+do
+    read -p "Choix: " resultat
+done
