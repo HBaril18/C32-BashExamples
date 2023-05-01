d492c0ab1ac6ac7c29365752df463cbae51db9a0
Henrick Baril <Barilhenrick@gmail.com>
2023-05-01 10:23:33 -0400

    ajout de code

--- 04-condition.sh
+++ 04-condition.sh
@@ -1,9 +1,34 @@
 #!/bin/bash
 
+note=60
 
+# if test ...
+# if [...]
 
+if [[ $note -lt 60 ]]
+then 
+    echo "Désoler ... vous échouez"
+fi
 
+elif test $note -eq 60 # elif -> permet de mettre un else avec le if
+then
+    echo "Ouf !"
+else                   # else -> pas besoin de "then"
+    echo "Beau travail !"
+fi
 
+read -p "Entrez une lettre" lettre
 
+case $lettre in
+    [[:lower:]])       # [[]] -> seulement nécessaire pour les lower, upper , etc...
+        echo "la lettre est en minuscule"
+        ;;             # ;; -> c'est un break
+    *)                 # *  -> c'est un autre (DEFAULT)
+        echo "Autre"
+        ;;
+esac
 
-
+if [[ $lettre != "a"]] # Pour les comparaison avec les lettres c'est comme en c++
+then    
+    echo "différent de a"
+fi
