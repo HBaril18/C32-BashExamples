3978ef6f20123643dd9fcdb7386c5149841bf2e5
Henrick Baril <Barilhenrick@gmail.com>
2023-05-02 12:48:09 -0400

    ajout code

--- 06-tableau.sh
+++ 06-tableau.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 declare -a film=(The Matrix)
-echo $(film[0])
-echo $(film[1])
-echo "Nombre elements : " $(film[@])
+echo ${film[0]}
+echo ${film[1]}
+echo "Nombre elements : " ${film[@]}
--- LAB01-8.sh
+++ LAB01-8.sh
@@ -1,2 +1,7 @@
 #!/bin/bash
 
+read -p "Entrez un chiffre de votre choix : " nb1
+read -p "Entrez un autre chiffre de votre choix : " nb2
+
+valeur=$(($nb1+$nb2)) # Permet de le mettre dans une variable
+echo $valeur
