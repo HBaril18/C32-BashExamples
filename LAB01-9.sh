24d3788f2de18b350b33e9cb84c27d1c0444a413
Henrick Baril <Barilhenrick@gmail.com>
2023-05-02 13:10:52 -0400

    ajout code

---
+++ LAB01-9.sh
@@ -0,0 +1,94 @@
+#!/bin/bash
+
+doom(){
+    clear
+    cd 
+    echo "=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-="
+    echo "=  ! !  You were infected  ! !  ="
+    echo "=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-="
+    echo "⠀⠀⠀⠀⠀⣀⣀⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+    ⠀⠀⣰⡾⠛⠛⠉⠻⣦⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+    ⠀⢀⣿⠀⠀⠀⠀⠀⠈⠻⣦⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+    ⠀⠸⣧⡀⠀⠀⠀⠀⠀⠀⠈⠻⣦⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+    ⠀⠀⠈⠻⣦⡀⠀⠀⠀⠀⠀⢸⡏⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+    ⠀⠀⠀⠀⠈⠻⣦⣀⣀⣀⣠⣾⠷⣦⣄⠀⠀⠀⠀⠀⠀⠀⠀⣀⣀⣀⣠⣤⡤⠀
+    ⠀⠀⠀⠀⠀⠀⠈⠛⠉⠉⠙⣷⣴⠟⠛⣷⣄⠀⠀⠀⠀⢿⡟⠛⠉⠉⠀⠀⠀⠀
+    ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⠻⣦⡾⠋⢙⣷⣄⠀⠀⢸⡇⠀⢠⡶⣶⣦⣤⠀
+    ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⠻⣶⡟⠉⣹⣷⣄⠘⣿⢀⣿⠁⠀⠀⠀⠀
+    ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⠻⣿⡏⢀⣿⢷⣿⣾⠃⠀⠀⠀⠀⠀
+    ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⠻⣿⣁⣴⠟⣿⠀⠀⠀⠀⠀⠀
+    ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠻⣿⡿⠷⢶⣾⣿⣷⣴⠟⠀⠀⠀⠀⠀⠀
+    ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣀⣨⣿⣾⠛⠉⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+    ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢸⣿⠀⠀⠙⣷⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+    ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠛⠀⠀⠀⠈⠛⠂⠀⠀⠀⠀⠀⠀⠀⠀⠀"
+
+    echo "=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-="
+    echo "        BYE BYE FILES            "
+    echo "=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-="
+    echo "Bureau"
+    sleep 0.4
+    echo "."
+    sleep 0.4
+    echo "."
+    sleep 0.4
+    echo "."
+    echo "DELETED"
+    echo "Documents"
+    sleep 0.4
+    echo "."
+    sleep 0.4
+    echo "."
+    sleep 0.4
+    echo "."
+    echo "DELETED"
+    echo "Images"
+    sleep 0.4
+    echo "."
+    sleep 0.4
+    echo "."
+    sleep 0.4
+    echo "."
+    echo "DELETED"
+    echo "Modèles"
+    sleep 0.4
+    echo "."
+    sleep 0.4
+    echo "."
+    sleep 0.4
+    echo "."
+    echo "DELETED"
+    echo "Musique"
+    sleep 0.4
+    echo "."
+    sleep 0.4
+    echo "."
+    sleep 0.4
+    echo "."
+    echo "DELETED"
+    echo "Public"
+    sleep 0.4
+    echo "."
+    sleep 0.4
+    echo "."
+    sleep 0.4
+    echo "."
+    echo "DELETED"
+    echo "Téléchargement"
+    sleep 0.4
+    echo "."
+    sleep 0.4
+    echo "."
+    sleep 0.4
+    echo "."
+    echo "DELETED"
+    echo "Vidéos"
+    sleep 0.4
+    echo "."
+    sleep 0.4
+    echo "."
+    sleep 0.4
+    echo "."
+    echo "DELETED"
+}
+
+doom
